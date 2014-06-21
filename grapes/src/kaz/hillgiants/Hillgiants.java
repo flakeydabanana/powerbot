f53dae708d3347a77806c8e58c352ff675b7ff6f
flakeydabanana <flakeydabanana@gmail.com>
2014-06-21 23:13:05 +0100

    v1.9

--- grapes/src/kaz/hillgiants/Hillgiants.java
+++ grapes/src/kaz/hillgiants/Hillgiants.java
@@ -5,7 +5,9 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
-import kaz.hillgiants.task.Task;
+import kaz.hillgiants.combat.*;
+import kaz.hillgiants.obstacles.*;
+import kaz.hillgiants.task.*;
 import kaz.hillgiants.walking.*;
 
 import org.powerbot.script.PaintListener;
@@ -14,7 +16,8 @@ import org.powerbot.script.Script.Manifest;
 import org.powerbot.script.rt6.ClientContext;
 
 @Manifest(description = "Kills hill giants in the Edgevill dungeon", name = "Giant Killer")
-public class Hillgiants extends PollingScript<ClientContext> implements PaintListener {
+public class Hillgiants extends PollingScript<ClientContext> implements
+		PaintListener {
 
 	private List<Task> taskList = new ArrayList<Task>();
 
@@ -23,12 +26,11 @@ public class Hillgiants extends PollingScript<ClientContext> implements PaintLis
 	@Override
 	public void start() {
 		taskList.add(new Togiants(ctx)); // testing needed
-
-
-		paintImage = downloadImage("http://i.imgur.com/lHX0Z47.png");
-
+		taskList.add(new Attack(ctx)); // testing needed
+		taskList.add(new Run(ctx)); // testing needed
+		taskList.add(new Enterdungen(ctx)); // testing needed
+		taskList.add(new Tobank(ctx)); // testing needed
 		return;
-
 	}
 
 	@Override
@@ -36,16 +38,14 @@ public class Hillgiants extends PollingScript<ClientContext> implements PaintLis
 		for (Task task : taskList) {
 			if (task.activate()) {
 				task.execute();
-
 			}
-
 		}
 		return;
 	}
 
 	@Override
 	public void repaint(Graphics arg0) {
-		// TODO Auto-generated method stub
-		
+		// to be done later
+
 	}
 }
